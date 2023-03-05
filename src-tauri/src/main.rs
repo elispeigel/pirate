3e5432c5d75c1fd8ac4e9d019ecba573fa58a39f
Eli Speigel <eli.speigel@gmail.com>
2023-03-04 19:48:19 -0800

    add file read

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,13 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+
+mod read_Torrent;
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+    // This is where you pass in your commands
+        .invoke_handler(tauri::generate_handler![read_Torrent::read_torrent_file])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,42 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+  const [fileContents, setFileContents] = useState<Uint8Array>();
+  const [filePath, setFilePath] = useState("");
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
-  }
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    console.log("🚀 ~ file: App.tsx:11 ~ handleFileChange ~ file:", file)
+    if (file) {
+      const fileUrl = URL.createObjectURL(file);
+      console.log("🚀 ~ file: App.tsx:13 ~ handleFileChange ~ fileUrl:", fileUrl)
+      setFilePath(fileUrl);
+    }
+  };
+
+  const handleClick = async () => {
+    try {
+      const result: Uint8Array = await invoke("read_torrent_file", {path: filePath});
+      console.log("🚀 ~ file: App.tsx:17 ~ handleClick ~ result:", result)
+      setFileContents(result);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <div className="container">
       <h1>Welcome to Tauri!</h1>
 
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div>
+        <input type="file" onChange={handleFileChange} />
+        <button onClick={handleClick} disabled={!filePath}>
+          Read File
+        </button>
+        <pre>{fileContents}</pre>
       </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <div className="row">
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            greet();
-          }}
-        >
-          <input
-            id="greet-input"
-            onChange={(e) => setName(e.currentTarget.value)}
-            placeholder="Enter a name..."
-          />
-          <button type="submit">Greet</button>
-        </form>
-      </div>
-      <p>{greetMsg}</p>
     </div>
   );
 }
