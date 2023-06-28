acd487d30c7bb0988354bbe5044fe37c81173e81
Eli Speigel <eli.speigel@gmail.com>
2023-06-27 19:31:32 -0700

    feat: updates

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,13 +1,33 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod hash;
+mod decoder;
+mod parser;
+mod tracker;
+mod peer_connection;
+mod message;
 
-mod read_torrents;
+use std::fs;
+
+#[tauri::command]
+fn start() {
+    const TORRENT_PATH: &str = "/Users/elispeigel/code/pirate/puppy.torrent";
+    
+    let bencoded_metadata: Vec<u8> = fs::read(TORRENT_PATH).unwrap();
+    let metadata = parser::parse_bencoded_torrent(bencoded_metadata).unwrap();
+    let peers = match tracker::get_peers(&metadata) {
+        Ok(peers) => peers,
+        Err(e) => panic!("{}", e),
+    };
+    
+    peer_connection::connect_to_peers(&peers, &metadata);
+}
 
 fn main() {
     tauri::Builder::default()
     // This is where you pass in your commands
-        .invoke_handler(tauri::generate_handler![read_torrents::read_torrent_file])
+        .invoke_handler(tauri::generate_handler![start])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
