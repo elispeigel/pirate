9b4eb2d98179391f0d59975a689a963e7a7940f6
Eli Speigel <eli.speigel@gmail.com>
2023-06-27 19:29:39 -0700

    feat: add message functionality

---
+++ src-tauri/src/message.rs
@@ -0,0 +1,46 @@
+#[derive(Debug)]
+pub struct MessagePayload {
+    pub message_id: u8,
+    pub message: Message,
+    pub payload: Vec<u8>,
+}
+
+#[derive(Debug)]
+pub enum Message {
+    Choke,
+    Unchoke,
+    Interested,
+    NotInterested,
+    Have,
+    Bitfield(Vec<u8>),
+    Request,
+    Piece,
+    Cancel,
+    KeepAlive,
+}
+
+pub fn identify_message(message_id: u8, message_body: &[u8]) -> Message {
+    match message_id {
+        0 => Message::Choke,
+        1 => Message::Unchoke,
+        2 => Message::Interested,
+        3 => Message::NotInterested,
+        4 => Message::Have,
+        5 => Message::Bitfield(message_body.to_vec()),
+        6 => Message::Request,
+        7 => Message::Piece,
+        8 => Message::Cancel,
+        _ => panic!("Unknown message"),
+    }
+}
+
+pub fn message_handler(msg: Message) {
+    match msg {
+        Message::Bitfield(body) => bitfield_handler(body),
+        _ => panic!("Unimplemented handler for: {:?}", msg),
+    }
+}
+
+fn bitfield_handler(bitfield: Vec<u8>) {
+    println!("Bitfield: {:?}", bitfield);
+}
